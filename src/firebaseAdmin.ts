89058307f7f0892c84b33dc415ed32897513d7a0
tim-ay-1mb <tim.ay.1mb@gmail.com>
2024-11-15 06:42:02 +0800

    Update firebaseAdmin.ts
    Fixed: firebase admin connection

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -1,4 +1,4 @@
-import admin from "firebase-admin";
+import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
