be8156f4e343775b8c4f7bec990cb6936d334e7e
tim-ay-1mb <tim.ay.1mb@gmail.com>
2024-11-15 07:53:19 +0800

    Fixed connection to firebase

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -1,27 +1,3 @@
 import admin from "firebase-admin";
 
-if (!admin.apps.length) {
-  console.log(process.env.FIREBASE_PROJECT_ID);
-  const serviceAccount: any = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-  };
-
-  console.log(serviceAccount);
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    projectId: "solidify-9f2c4",
-  });
-}
-
 export default admin;
